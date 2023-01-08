3d6ff457ec21c04d02bd8470d5f5f204a5d1cf4a
Eddie Hakansson <eddan96@live.se>
2023-01-08 08:41:10 +0100

    changes

--- javascript.js
+++ javascript.js
@@ -9,98 +9,211 @@ Function that takes 2 parameters, computer value and user input value.
 function that plays round with these 2 functions, returns winner in console.log.
 
 
+1 counter for computer. 
+1 counter for human. 
+
+
+1 counter rounds played. 3 button for human. Rock, paper scissor. With image inside button.
+1 image in div for the choice human make.
+
+1 image in div for choice computer make.
+
 */
 
+const mainContainer = document.querySelector("#main-container")
+const mainGameContainer = document.querySelector("#main-game-container");
+const playContainer = document.querySelector("#play-container");
+const titleButtonContainer = document.querySelector("#title-button-container");
+const buttonContainer = document.querySelector("#button-container");
+const footerContainer = document.querySelector("#footer-container");
+const containerScore = document.querySelector("#score-container");
+const playButton = document.querySelector("#button-play");
+const titleText = document.querySelector("#title-text");
+const roundsInput = document.querySelector("#rounds-input");
+const totalRoundsP = document.querySelector("#p-numb-rounds");
+const roundsPlayedP = document.querySelector("#rounds-played");
+const aiChoiceImg = document.querySelector("#ai-choice-img");
+const startButton = document.querySelector("#start-button");
+const aiScoreParagraph = document.querySelector("#ai-score-p");
+const humanScoreParagraph = document.querySelector("#human-score-p");
 
 
-const choices = ["Rock", "Paper", "Scissors"]
-
-let computerScore = 0
-let humanScore = 0
-let rounds = 0
 
 
-function getComputerChoice() {
-    let randNumb = Math.floor(Math.random() * choices.length);
-    let computerChoice = choices[randNumb];
+const buttons = Array.from(document.querySelectorAll(".game-button"));
 
-    console.log(computerChoice)
-    return computerChoice
 
+// If user does not input integer between 1 and 10.
+const errorParagraph =document.createElement("p"); 
+errorParagraph.setAttribute("id", "error-rounds");
+errorParagraph.textContent = "Invalid(Pick number between 1-10).";
+
+
+
+let totalRounds = 0;
+let roundsPlayed = 0;
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+let arrayAiChoices = ["rock", "paper", "scissors"];
+let aiChoice = "";
+
+ 
+
+/* 
+when play button is clicked.
+Declares variable with the user input value.
+Checks for invalid inputs(NaN, 0 or less and higher than 10).
+if all checks return true, returns the input value.
+
+removes everything on current screen.
+Adds the hidden html.
+
+*/
+
+function removeTitle() {
+    playContainer.style.display = "none";
+    playButton.remove();
+    roundsInput.remove();
 }
 
-function getUserChoice() {
-    let userPrompt = prompt("Rock paper or scissors?");
-    let upperCase = userPrompt.charAt(0)
-    upperCase = upperCase.toUpperCase()
-    let lowerCase = userPrompt.slice(1)
-    lowerCase = lowerCase.toLowerCase()
-    
-    userPrompt = upperCase + lowerCase
-    
-    return userPrompt
+
+function getTotalRounds() {
+    if (!isNaN(roundsInput.value) && parseInt(roundsInput.value) > 0 && parseInt(roundsInput.value) <= 10) {
+        return parseInt(roundsInput.value);
+    }
+    const errorParagraph =document.createElement("p"); 
+    errorParagraph.setAttribute("id", "error-rounds");
+    errorParagraph.textContent = "Invalid(Pick number between 1-10).";
+    titleButtonContainer.appendChild(errorParagraph);
+    return null;
+}
+
+
+
+function displayMain() {
+    titleText.style.fontSize = "40px";
+    mainGameContainer.style.display = "flex";
+    totalRoundsP.textContent = `Number of rounds: ${totalRounds}`;
+    roundsPlayedP.textContent = `Rounds played: ${roundsPlayed}`;
+
     
 }
 
 /*
-This function will call both functions to a variable each.
-Then compare these two variables.
 
-if computer or human has rock, and computer or human has scissor. computer or human wins.
-A loop that loops through the choices. Every loop checks the computers choice.
+Function checks if totalRounds is not null.
+Executes declared functions.
+
 */
-function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getUserChoice();
-    
-    if (computerChoice == humanChoice) {
-        console.log("Draw!")
-    } else {
-        if (computerChoice == "Rock" && humanChoice != "Paper") {
-            ++computerScore
-            ++rounds
-            console.log("Computer Wins!")
-            console.log("Computer score: " + computerScore + ", " + "Human score: " + humanScore + ".")
-            console.log("Rounds played: " + rounds + ".")
-        } else if (computerChoice == "Scissors" && humanChoice != "Rock") {
-            ++computerScore
-            ++rounds
-            console.log("Computer Wins!")
-            console.log("Computer score: " + computerScore + ", " + "Human score: " + humanScore + ".")
-            console.log("Rounds played: " + rounds + ".")
-        } else if (computerChoice == "Paper" && humanChoice != "Scissors") {
-            ++computerScore
-            ++rounds
-            console.log("Computer Wins!")
-            console.log("Computer score: " + computerScore + ", " + "Human score: " + humanScore + ".")
-            console.log("Rounds played: " + rounds + ".")
-        } else {
-            ++humanScore
-            ++rounds
-            console.log("Human Wins!")
-            console.log("Computer score: " + computerScore + ", " + "Human score: " + humanScore + ".")
-            console.log("Rounds played: " + rounds + ".")
 
-        }
+function clickedPlay(event) {
+    totalRounds = getTotalRounds();
+    if (totalRounds) {
+        removeTitle();
+        displayMain();
     }
+}
+playButton.addEventListener("click", clickedPlay);
+
+
+
+/*
+
+Game loop.
+When Start button is pressed.
+
+When start button is pressed, reset all scores, and rounds played to 0.
+this loop should run while rounds played is less than total rounds.
+Should break game loop if Reset button is pressed.
+
+
+
+*/
+
+
+function choiceAi() {
+    // Function to simulate the ai choosing Rock paper or scissors.
+    // Count random between 1-3 seconds. 
+    // When time is passed, replace the loading gif with the choice.    
+    let randomNumber = Math.floor(Math.random() * 2) + 1;
+    return arrayAiChoices[randomNumber];
+
+}
+
+
+
+
+function choiceHuman(e) {
+    /* 
+    When one of choices is pressed.
+    */
+    if (e.target.tagName !== "IMG") {
+        
+        return e.target.textContent.toLowerCase();
+    }   
+    else {
+        return e.target.alt;
+
+    }   
+
+}
+
+
+
+
+
+function addScoreAi() {
+
+
+}
+
+
+
+
+
+
+
+
+function clickedStart() {
+    aiChoiceImg.style.display = "flex";
+    aiScoreParagraph.style.display = "block";
+    humanScoreParagraph.style.display = "block";
+    startButton.remove();
     
 }
 
-while (rounds != 5 && computerScore != 3 && humanScore != 3) {
-    playRound()
 
-};
 
-if (computerScore > humanScore) {
-    console.log("The Computer Wins the game!")
-    
-    }else if (computerScore == humanScore) {
-        console.log("This game is a draw!")
-        
-    } else {
-        console.log("You win!")
+
+function battle(e) {
+    /*
+
+    When one of the choices is clicked, run this function.
+    Calls humanChoice function, returns string.
+
+
+    */
+
+    let humanChoice = choiceHuman(e);
+    aiChoice = choiceAi();
+
+    if(humanChoice === "rock" && aiChoice === "scissors") {
+
     }
 
-/*tjeeena
-*/
+
+}
+
+
+
+
+
+
+startButton.addEventListener("click", clickedStart);
+buttons.forEach(btn => btn.addEventListener("click", battle));
+
+
 

35b1dbd5e2e6641130f4726f397339a11086e137
Eddie Hakansson <eddan96@live.se>
2023-01-09 04:56:20 +0100

    Done for now

--- javascript.js
+++ javascript.js
@@ -28,14 +28,17 @@ const buttonContainer = document.querySelector("#button-container");
 const footerContainer = document.querySelector("#footer-container");
 const containerScore = document.querySelector("#score-container");
 const playButton = document.querySelector("#button-play");
+const resetButton = document.querySelector("#reset-button");
 const titleText = document.querySelector("#title-text");
 const roundsInput = document.querySelector("#rounds-input");
-const totalRoundsP = document.querySelector("#p-numb-rounds");
+const toWinScoreP = document.querySelector("#p-numb-win-score");
 const roundsPlayedP = document.querySelector("#rounds-played");
 const aiChoiceImg = document.querySelector("#ai-choice-img");
 const startButton = document.querySelector("#start-button");
 const aiScoreParagraph = document.querySelector("#ai-score-p");
 const humanScoreParagraph = document.querySelector("#human-score-p");
+const whoWonParagraph = document.querySelector("#who-won-p");
+const loadingImg = document.querySelector("#img-loading");
 
 
 
@@ -44,13 +47,13 @@ const buttons = Array.from(document.querySelectorAll(".game-button"));
 
 
 // If user does not input integer between 1 and 10.
-const errorParagraph =document.createElement("p"); 
+const errorParagraph = document.createElement("p"); 
 errorParagraph.setAttribute("id", "error-rounds");
-errorParagraph.textContent = "Invalid(Pick number between 1-10).";
+titleButtonContainer.appendChild(errorParagraph);
 
 
 
-let totalRounds = 0;
+let winScore = 0;
 let roundsPlayed = 0;
 
 let humanScore = 0;
@@ -61,6 +64,13 @@ let arrayAiChoices = ["rock", "paper", "scissors"];
 let aiChoice = "";
 
  
+let clickedPlayCheck = null;
+let humanWinCheck = null;
+let computerWinCheck = null;
+let gameOverCheck = null;
+
+let iconTimer = null;
+
 
 /* 
 when play button is clicked.
@@ -80,14 +90,11 @@ function removeTitle() {
 }
 
 
-function getTotalRounds() {
+function getWinScore() {
     if (!isNaN(roundsInput.value) && parseInt(roundsInput.value) > 0 && parseInt(roundsInput.value) <= 10) {
         return parseInt(roundsInput.value);
     }
-    const errorParagraph =document.createElement("p"); 
-    errorParagraph.setAttribute("id", "error-rounds");
     errorParagraph.textContent = "Invalid(Pick number between 1-10).";
-    titleButtonContainer.appendChild(errorParagraph);
     return null;
 }
 
@@ -96,11 +103,19 @@ function getTotalRounds() {
 function displayMain() {
     titleText.style.fontSize = "40px";
     mainGameContainer.style.display = "flex";
-    totalRoundsP.textContent = `Number of rounds: ${totalRounds}`;
+    toWinScoreP.textContent = `Score to win: ${winScore}`;
     roundsPlayedP.textContent = `Rounds played: ${roundsPlayed}`;
 
     
 }
+
+function updateScore() {
+    toWinScoreP.textContent = `Score to win: ${winScore}`;
+    roundsPlayedP.textContent = `Rounds played: ${roundsPlayed}`;
+    humanScoreParagraph.textContent = `Your score: ${humanScore}`;
+    aiScoreParagraph.textContent = `AI score: ${computerScore}`;   
+}
+
 
 /*
 
@@ -110,8 +125,8 @@ Executes declared functions.
 */
 
 function clickedPlay(event) {
-    totalRounds = getTotalRounds();
-    if (totalRounds) {
+    winScore = getWinScore();
+    if (winScore) {
         removeTitle();
         displayMain();
     }
@@ -134,15 +149,51 @@ Should break game loop if Reset button is pressed.
 */
 
 
-function choiceAi() {
+function choiceAi(e) {
     // Function to simulate the ai choosing Rock paper or scissors.
     // Count random between 1-3 seconds. 
     // When time is passed, replace the loading gif with the choice.    
-    let randomNumber = Math.floor(Math.random() * 2) + 1;
-    return arrayAiChoices[randomNumber];
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if(!gameOverCheck) {
+        aiIconChange(arrayAiChoices[randomNumber]);
+        return arrayAiChoices[randomNumber];
+    }
 
 }
 
+
+/*
+
+Function that changes icon for the AI depending on the choice.
+
+*/
+
+function aiIconChange(selection) {
+
+	    if (selection === "rock") {
+	        loadingImg.src = "images/icons8-rock-50.png";
+	    }
+	    else if (selection === "paper") {
+	        loadingImg.src = "images/icons8-papers-64.png";
+	    }
+	    else if (selection === "scissors"){
+	        loadingImg.src = "images/icons8-scissor-96.png";
+	    }
+        if(computerWinCheck) {
+            loadingImg.src = "images/icons8-fairytale-50.png";
+        }
+	        
+}
+
+
+
+
+
+
+function aiIconLoading() {
+    loadingImg.src = "images/icons8-loading-circle.gif";
+}
 
 
 
@@ -150,30 +201,17 @@ function choiceHuman(e) {
     /* 
     When one of choices is pressed.
     */
-    if (e.target.tagName !== "IMG") {
-        
-        return e.target.textContent.toLowerCase();
-    }   
-    else {
-        return e.target.alt;
 
-    }   
-
+if (!gameOverCheck) {
+        if (e.target.tagName !== "IMG") {
+            
+            return e.target.textContent.toLowerCase();
+        }   
+        else {
+            return e.target.alt;
+        }   
+    }  
 }
-
-
-
-
-
-function addScoreAi() {
-
-
-}
-
-
-
-
-
 
 
 
@@ -181,10 +219,34 @@ function clickedStart() {
     aiChoiceImg.style.display = "flex";
     aiScoreParagraph.style.display = "block";
     humanScoreParagraph.style.display = "block";
-    startButton.remove();
+    whoWonParagraph.style.display = "block";
+    startButton.style.display = "none";
+
+    clickedPlayCheck = true;
+
     
 }
 
+
+function resetGame (e) {
+    computerScore = 0;
+    humanScore = 0;
+    roundsPlayed = 0;
+    clickedPlayCheck = null;
+    gameOverCheck = null;
+    humanWinCheck = null;
+    computerWinCheck = null;
+    updateScore();
+
+    aiChoiceImg.style.display = "none";
+    aiScoreParagraph.style.display = "none";
+    humanScoreParagraph.textContent = "";
+    startButton.style.display = "block";
+    whoWonParagraph.style.display = "none";
+    whoWonParagraph.textContent = "";
+    loadingImg.src = "images/icons8-loading-circle.gif";
+}
+ 
 
 
 
@@ -194,18 +256,63 @@ function battle(e) {
     When one of the choices is clicked, run this function.
     Calls humanChoice function, returns string.
 
-
     */
 
-    let humanChoice = choiceHuman(e);
-    aiChoice = choiceAi();
-
-    if(humanChoice === "rock" && aiChoice === "scissors") {
-
+    if (!gameOverCheck && clickedPlayCheck) {
+	        if((winScore !== humanScore) || (winScore !== computerScore)) {
+	            let humanChoice = choiceHuman(e);
+	            aiChoice = choiceAi(e);
+	            console.log(`Human: ${humanChoice} AI: ${aiChoice}`);
+	            if(aiChoice === humanChoice) {
+	                whoWonParagraph.textContent = "Draw!";
+	                roundsPlayed += 1;
+	                updateScore();
+	            }
+	            else if(aiChoice === "rock" && humanChoice !== "paper") {
+	                whoWonParagraph.textContent = "Computer Wins!";
+	                computerScore += 1;
+	                roundsPlayed += 1;
+	                updateScore();
+	            }
+	            else if (aiChoice === "paper" && humanChoice !== "scissors") {
+	                whoWonParagraph.textContent = "Computer Wins!";
+	                computerScore += 1;
+	                roundsPlayed += 1;
+	                updateScore();
+	            }
+	            else if (aiChoice === "scissors" && humanChoice !== "rock") {
+	                whoWonParagraph.textContent = "Computer Wins!";
+	                computerScore += 1;
+	                roundsPlayed += 1;
+	                updateScore();
+	            }else {
+	                whoWonParagraph.textContent = "You Win!";
+	                humanScore += 1;
+	                roundsPlayed += 1;
+	                updateScore();
+	            }
+	        }
     }
+        
+            if ((computerScore > humanScore) && (computerScore === winScore)) {    
+                whoWonParagraph.style.fontSize = "25px";
+                whoWonParagraph.textContent = "The Computer Wins it all!";
+                computerWinCheck = true;
+                gameOverCheck = true;
+                aiIconChange(aiChoice);
+                    
+            }
+            else if ((humanScore > computerScore) && (humanScore === winScore)) {
+                whoWonParagraph.style.fontSize = "25px";
+                whoWonParagraph.textContent = "You Win the game!";
+                humanWinCheck = true;
+                gameOverCheck = true;
+                aiIconChange(aiChoice);
 
-
+            }
+        
 }
+
 
 
 
@@ -214,6 +321,6 @@ function battle(e) {
 
 startButton.addEventListener("click", clickedStart);
 buttons.forEach(btn => btn.addEventListener("click", battle));
-
+resetButton.addEventListener("click", resetGame);
 
 
